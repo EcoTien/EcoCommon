d7e8db9c1382a7152cdd8b2ab1c80dde310c7b26
EcoMineVN <tiendv@jacat.games>
2024-02-24 18:54:47 +0700

    update folder + add scene wrapper

--- Common/Utils/ScreenUtils.cs
+++ Common/Utils/ScreenUtils.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 namespace JacatGames.Common.Utils
@@ -7,24 +6,31 @@ namespace JacatGames.Common.Utils
     {
         private const int screenHeightBase = 640;
         private const int screenWidthBase = 360;
-        
+
         public static float GetRateMutiply()
         {
-            Vector2 referenceResolution = new Vector2(screenHeightBase, screenWidthBase); 
-            Vector2 screenSize = new Vector2(Screen.height, Screen.width); 
+            Vector2 referenceResolution = new Vector2(screenHeightBase, screenWidthBase);
+            Vector2 screenSize = new Vector2(Screen.height, Screen.width);
 
             float rateCanvas = referenceResolution.x / referenceResolution.y;
             float rateScreen = screenSize.x / screenSize.y;
 
             return rateScreen / rateCanvas;
         }
-        
+
         public static Vector2 ConvertWorldToScreenPoint(RectTransform parentRect, Vector3 pos, Camera _camera)
         {
             var screenPoint = _camera.WorldToScreenPoint(pos);
             RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRect, screenPoint, _camera,
                 out var localPoint);
             return localPoint;
+        }
+
+        public static Vector2 GetRectSizeDelta(RectTransform rectTransform)
+        {
+            float rectWidth = (rectTransform.anchorMax.x - rectTransform.anchorMin.x) * Screen.width;
+            float rectHeight = (rectTransform.anchorMax.y - rectTransform.anchorMin.y) * Screen.height;
+            return new Vector2(rectWidth, rectHeight);
         }
     }
 }
---
+++ Common/Wrapper/SceneManagerWrapper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace HITStudio.SceneManager
+{
+    public class SceneManagerWrapper
+    {
+        private static readonly Dictionary<string, object> _scenedata = new();
+        
+        public static UniTask Load(string sceneName, object data = null)
+        {
+            return LoadScene(sceneName, LoadSceneMode.Single, data);
+        }
+
+        public static UniTask Add(string sceneName, object data = null)
+        {
+            return LoadScene(sceneName, LoadSceneMode.Additive, data);
+        }
+
+        private static async UniTask LoadScene(string sceneName, LoadSceneMode loadSceneMode, object data = null)
+        {
+            // Check scene is loaded
+            if (IsSceneLoaded(sceneName))
+                throw new Exception($"Scene {sceneName} is loaded.");
+            
+            _scenedata.Add(sceneName, data); // Save Data
+            
+            AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, loadSceneMode);
+            await UniTask.WaitUntil(() => asyncOperation.isDone);
+            
+        }
+
+        public static async UniTask Close(string sceneName)
+        {
+            // Check scene is loaded
+            if (!IsSceneLoaded(sceneName))
+                throw new NullReferenceException($"Scene {sceneName} has not been loaded.");
+            
+            AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName);
+            await UniTask.WaitUntil(() => asyncOperation.isDone);
+            
+            _scenedata.Remove(sceneName); // Remove Data
+        }
+
+        public static T GetData<T>(string sceneName)
+        {
+            if (!HasData(sceneName))
+                throw new NullReferenceException($"Can't get scene {sceneName} data.");
+            
+            return (T)_scenedata[sceneName];
+        }
+
+        public static bool HasData(string sceneName)
+        {
+            if (!_scenedata.ContainsKey(sceneName))
+                return false;
+            if (_scenedata[sceneName] == null)
+                return false;
+            return true;
+        }
+
+        private static bool IsSceneLoaded(string sceneName)
+        {
+            Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName(sceneName);
+            return scene.isLoaded || scene.IsValid();
+        }
+    }
+}
