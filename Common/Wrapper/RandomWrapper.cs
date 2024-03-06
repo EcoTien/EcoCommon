018ecf8fd8de80bf7cb499af716dc2b08845fc44
EcoMineVN <tiendv@jacat.games>
2024-03-07 00:10:09 +0700

    add random wrapper

---
+++ Common/Wrapper/RandomWrapper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace JacatGames.Common.Utils
+{
+    public static class RandomWrapper
+    {
+        public static bool Rate(float rate)
+        {
+            float random = UnityEngine.Random.Range(0f, 100f);
+            return rate >= random;
+        }
+    }
+}
