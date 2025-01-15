1c33c7dfb8cb99550eee55d8ed5d8463ae8c9d68
Tien Company <tiendv@blackgemsglobal.com>
2025-01-15 17:19:19 +0700

    add extensions

---
+++ Common/Extensions/TextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace EcoMine.Common.Extensions
+{
+    public static class TextExtensions
+    {
+        public static string FormatNumber(this int number)
+        {
+            NumberFormatInfo numberFormat = new NumberFormatInfo();
+            numberFormat.NumberGroupSeparator = ".";
+            return number.ToString("N0", numberFormat);
+        }
+        
+        public static string FormatNumber(this float number)
+        {
+            NumberFormatInfo numberFormat = new NumberFormatInfo();
+            numberFormat.NumberGroupSeparator = ".";
+            return number.ToString("N0", numberFormat);
+        }
+    }
+}
